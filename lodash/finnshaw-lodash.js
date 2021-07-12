e25b45a348b7ec6a32d62002d2b95e8520d02ec0
picco <finn.shawchn@gmail.com>
2021-07-12 15:40:57 +0800

    f chunk

--- lodash/finnshaw-lodash.js
+++ lodash/finnshaw-lodash.js
@@ -1,5 +1,17 @@
 var finnshaw = function () {
-  
+
+  function chunk(arr, size) {
+    let res = []
+    let chunkArr = []
+    for (let i = 0; i < arr.length; i++) {
+      chunkArr.push(arr[i])
+      if (i % (size - 1) === 0) {
+        res.push(chunkArr)
+        chunkArr = []
+      }
+    }
+    return res
+  }
 
 
   return {
