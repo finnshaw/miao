4cbabbffc90dc0c524cdeb8aa919da0ed2bd7f2e
picco <finn.shawchn@gmail.com>
2021-07-12 22:03:25 +0800

    f map & forEach & concat

--- lodash/finnshaw-lodash.js
+++ lodash/finnshaw-lodash.js
@@ -28,6 +28,20 @@ var finnshaw = function () {
     return res
   }
 
+  //concat
+  function concat(arr, vals) {
+    let res = arr.slice()
+    for(let i = 1; i < arguments.length; i++) {
+      let item = arguments[i]
+      if(Array.isArray(item)) {
+        res.push(...item)
+      } else {
+        res.push(item)
+      }
+    }
+    return res
+  }
+
   //uniq
   function uniq(arr) {
     let res = []
@@ -50,6 +64,21 @@ var finnshaw = function () {
     return res
   }
 
+  //flatten
+  function flatten(arr) {
+    let res = []
+    for(let i = 0; i < arr.length; i++) {
+      let e = arr[i]
+      if(Array.isArray(e)) {
+        for(let j = 0; j < e.length; j++) {
+          res.push(e[j])
+        }
+      } else {
+        res.push(e)
+      }
+    }
+    return res
+  }
   //flattenDeep
   function flattenDeep(arr) {
     let res = []
@@ -86,14 +115,40 @@ var finnshaw = function () {
     return res
   }
 
+   //forEach
+   function forEach(coll, action) {
+    for(let key in coll) {
+      action(coll[key], key)
+    }
+    return coll
+  }
+
+  //map
+  function map(coll, f) {
+    let mapped = []
+    if(typeof f === 'function') {
+      forEach(coll, (item, key) => {
+        mapped.push(f(item))
+      })
+    } else {
+      forEach(coll,(item, key) => {
+        mapped.push(item)
+      })
+    }
+    return mapped
+  }
 
 
 
   return {
     chunk: chunk,
     compact: compact,
+    concat: concat,
     uniq: uniq,
+    flatten: flatten,
     flattenDeep: flattenDeep,
     flattenDepth: flattenDepth,
+    forEach: forEach,
+    map: map,
   }
 }()
